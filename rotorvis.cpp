09a226c904a5d357df2f0c996bdf4f1d2024ac67
Martin Grap <rmsk2@gmx.de>
2016-06-27 23:46:28 +0200

    Corrected license header

--- rotorvis.cpp
+++ rotorvis.cpp
@@ -14,22 +14,6 @@
  * limitations under the License.
  ***************************************************************************/
 
-/***************************************************************************
- * Copyright 2015 Martin Grap
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ***************************************************************************/
-
 /*! \file rotorvis.cpp
  *  \brief This file contains a class that implements all the user interface stuff that is provided on top of the basic KL7, SIGABA, Typex, SG39 and Nema simulator functionality.
  */
