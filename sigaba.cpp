4153ca1f940fcd697ac56028e3182ef785fffe07
Martin Grap <rmsk2@gmx.de>
2018-04-15 22:12:57 +0200

    Updated license headers.

--- sigaba.cpp
+++ sigaba.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright 2017 Martin Grap
+ * Copyright 2018 Martin Grap
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
